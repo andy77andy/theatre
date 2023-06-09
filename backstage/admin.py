97ad69364a6f1e6155e946574f52a7818dd82b45
Andy <andy77andy@ukr.net>
2023-06-10 02:04:45 +0300

    Register model Award in Admin Panel and create AwardAdmin class with list_filter

--- backstage/admin.py
+++ backstage/admin.py
@@ -1,42 +1,48 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Actor
+from .models import Actor, Award, Director, Play, Genre
 
 
 @admin.register(Actor)
 class ActorAdmin(UserAdmin):
-    list_display = UserAdmin.list_display + ("average_fee", "awards",)
+    list_display = UserAdmin.list_display + ("average_fee", )
     fieldsets = UserAdmin.fieldsets + (
         (("Additional info", {"fields": ("average_fee", "awards",)}),)
     )
-    #
-    # add_fieldsets = UserAdmin.add_fieldsets + (
-    #     (
-    #         (
-    #             "Additional info",
-    #             {
-    #                 "fields": (
-    #                     "email",
-    #                     "first_name",
-    #                     "last_name",
-    #                     "average_fee",
-    #                     "awards",
-    #                 )
-    #             },
-    #         ),
-    #     )
-    # )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "email",
+                        "first_name",
+                        "last_name",
+                        "average_fee",
+                        "awards",
+                    )
+                },
+            ),
+        )
+    )
 
 
-# @admin.register(Post)
-# class PostAdmin(admin.ModelAdmin):
-#     search_fields = ("title",)
+@admin.register(Award)
+class AwardAdmin(admin.ModelAdmin):
+    list_filter = ("year",)
+
+
+# @admin.register(Director)
+# class DirectorAdmin(admin.ModelAdmin):
+#     search_fields = ("last_name",)
 #
 #
-# @admin.register(Commentary)
-# class CommentaryAdmin(admin.ModelAdmin):
-#     search_fields = ("post",)
-#     list_filter = ("created_time",)
-# from django.contrib import admin
+# @admin.register(Play)
+# class PlayAdmin(admin.ModelAdmin):
+#     search_fields = ("name",)
 #
-# # Register your models here.
+#
+# @admin.register(Genre)
+# class PlayAdmin(admin.ModelAdmin):
+#     pass
--- backstage/models.py
+++ backstage/models.py
@@ -8,7 +8,7 @@ from theatre import settings
 class Award(models.Model):
     name = models.CharField(max_length=255)
     nomination = models.CharField(max_length=255, null=True, blank=True)
-    year = models.CharField(max_length=255)
+    year = models.IntegerField()
 
     def __str__(self):
         return f"{self.name} ({self.nomination}, {self.year})"
@@ -16,10 +16,14 @@ class Award(models.Model):
 
 class Actor(AbstractUser):
     average_fee = models.CharField(max_length=255)
-    award = models.ManyToManyField(Award, related_name="actor_awards")
+    awards = models.ManyToManyField(Award, related_name="actor_awards")
 
     def __str__(self):
         return f"{self.username} ({self.first_name} {self.last_name})"
+
+    class Meta:
+        verbose_name = "actor"
+        verbose_name_plural = "actors"
 
     # def get_absolute_url(self):
     #     return reverse("taxi:driver-detail", kwargs={"pk": self.pk})
@@ -28,7 +32,7 @@ class Actor(AbstractUser):
 class Director(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    award = models.ManyToManyField(Award, related_name="director_awards")
+    awards = models.ManyToManyField(Award, related_name="director_awards")
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
@@ -39,15 +43,16 @@ class Play(models.Model):
     troupe = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="actors")
     on_stage = models.BooleanField()
     day_of_premiere = models.DateTimeField(auto_now=True)
+    description = models.TextField(default="Best ever play")
     director = models.ForeignKey(Director, on_delete=models.DO_NOTHING)
-    award = models.ManyToManyField(Award, related_name="awards")
+    awards = models.ManyToManyField(Award, related_name="awards")
 
     class Meta:
         ordering = ["day_of_premiere"]
 
     def __str__(self):
-        if self.award:
-            return f"{self.name}, director, {self.director}, awarded by {self.award}"
+        if self.awards:
+            return f"{self.name}, director, {self.director}, awarded by {self.awards}"
         return f"{self.name}, director, {self.director}"
 
 
