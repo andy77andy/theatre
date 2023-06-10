43de1e23be3fd307286eab8765c411aa0f050295
Andy <andy77andy@ukr.net>
2023-06-11 00:50:12 +0300

    In local urls create path to home page, named index

--- backstage/views.py
+++ backstage/views.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from backstage.models import Award, Actor, Director, Play
+
+
+# @login_required
+def index(request):
+
+    num_actors = Actor.objects.count()
+    num_directors = Director.objects.count()
+    num_plays = Play.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_users": num_actors,
+        "num_directors": num_directors,
+        "num_plays": num_plays,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "backstage/index.html", context=context)
