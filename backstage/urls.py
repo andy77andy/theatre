fe8967c37f5df68d15aa4e133b86ad834a5a7a1c
Andy <andy77andy@ukr.net>
2023-06-10 18:00:36 +0300

    Create an index function in views.py,using template, render and context, it will regard as a home page

--- backstage/urls.py
+++ backstage/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 
 # from theatre.views import index
 
-urlpatterns = [
-    path("", index, name="index"),
-]
+# urlpatterns = [
+#     path("", index, name="index"),
+# ]
 
 app_name = "backstage"
